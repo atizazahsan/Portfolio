2aaf5d0ec21a9cb1b59e7ebf0e6b7f035b9b19ed
Atizaz Ahsan <atizazahsan05@gmail.com>
2021-01-17 21:24:27 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menuIcon = document.querySelector('.mobile');
+const menuItem = document.querySelector('.showcase');
+const navLink = document.querySelector('.mobile-menu a');
+function displayNav() {
+    menuIcon.classList.toggle('active');
+    menuItem.classList.toggle('active');    
+}
+
+function addLinkEffect() {
+    // navLink.style.transform-property = "transform"
+    navLink.style.transform = "scale(1.2)"
+    navLink.style.transitionProperty= "transform"
+    navLink.style.transitionDuration = "2s";
+    // navLink.style.transform-delay = ".3s"
+}
+menuIcon.addEventListener('click', displayNav)
+menuIcon.addEventListener('click', addLinkEffect)
