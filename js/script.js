239258c5e8f00443e27f5ee4ef1b58e4e90238d3
Atizaz Ahsan <atizazahsan05@gmail.com>
2021-01-18 12:47:57 +0500

    About Section Added
    About us section designed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector('.mobile');
 const menuItem = document.querySelector('.showcase');
 const navLink = document.querySelector('.mobile-menu a');
+const sideNav = document.querySelector('.mobile-menu');
 function displayNav() {
     menuIcon.classList.toggle('active');
     menuItem.classList.toggle('active');    
@@ -11,6 +12,8 @@ function addLinkEffect() {
     navLink.style.transform = "scale(1.2)"
     navLink.style.transitionProperty= "transform"
     navLink.style.transitionDuration = "2s";
+    sideNav.classList.toggle('active')
+
     // navLink.style.transform-delay = ".3s"
 }
 menuIcon.addEventListener('click', displayNav)
